e4edae47e5c92ec84d9cf2df986d63c125cd8a20
birlibirloque <manuel.kstro@gmail.com>
2014-05-23 21:45:01 +0200

    My best solution in py3.2

--- python/string-calculator/TestStringCalculator.py
+++ python/string-calculator/TestStringCalculator.py
@@ -1,29 +1,37 @@
 import unittest
+import re
 
 def suma (string):
-	delim, sumandos = dameDelimitadorYSumandos(string)
-	chequeaSumandos(delim,sumandos)
-	return (sumaConDelimitador(delim,sumandos))
+	sumandos = dameSumandos(string)
+	chequeaSumandos(sumandos)
+	return sum(sumandos)
 
-def dameDelimitadorYSumandos(string):
-	if (not tieneDelimitadores(string)):
-		return ',', string.translate({ord('\n'):''})
-
-	delim = string.split('\n')[0].translate({ord(i):None for i in '/[]'}) 
-	sumandos = string.split('\n')[1]
-	return delim, sumandos
+def dameSumandos(string):
+	if (tieneDelimitadores(string)):
+		string = dameSumandosSinDelimitadores(string)
+	return [int(n) for n in string.strip('\n').split(',') if (n is not "") and (int(n) < 1000)]
 
 def tieneDelimitadores(string):
 	return string[:2] == '//'
 
-def chequeaSumandos(delim, sumandos):
-	negativos = []
-	if ("-" in sumandos):
-		negativos = [int(n) for n in sumandos.split(delim) if int(n) < 0]
-		raise Exception('negativos no soportados: ' + str(negativos))
+def dameSumandosSinDelimitadores(string):
+	s_delimitadores = string.split('\n',1)[0].strip('/')
+	l_delimitadores = re.findall(r'\[([^]]*)\]', s_delimitadores)
+	if (len(l_delimitadores) == 0):
+		l_delimitadores.append(s_delimitadores)
+	string = cambiaDelimitadoresPorComas(string,l_delimitadores)
+	return string
 
-def sumaConDelimitador (delim,string):
-	return sum([int(n) for n in string.split(delim) if (n is not "") and (int(n) < 1000)])
+def cambiaDelimitadoresPorComas(string,delimitadores):
+	string = string.split('\n',1)[1]
+	for d in delimitadores:
+		string = string.replace(d,',')
+	return string
+
+def chequeaSumandos(sumandos):
+	negativos = [n for n in sumandos if n < 0]
+	if (len(negativos)>0):
+		raise Exception('negativos no soportados: ' + str(negativos))
 
 class TestStringCalculator (unittest.TestCase):
 
@@ -43,19 +51,27 @@ class TestStringCalculator (unittest.TestCase):
 		self.assertEqual(6, suma("1,\n2,3"))
 
 	def testCadenaConDelimitador (self):
-		self.assertEqual(6, suma("//,\n1,2,3"))
+		self.assertEqual(6, suma("//:\n1:2:3"))
 
 	def testCadenaConNegativos (self):
-		try:
+		with self.assertRaises(Exception) as context:
 			suma("-1,2,3,-4")
-		except Exception as e:
-			self.assertEqual(str(e), 'negativos no soportados: [-1, -4]')
+		self.assertEqual(str(context.exception), 'negativos no soportados: [-1, -4]')
 
 	def testCadenaConNumerosGrandes (self):
 		self.assertEqual(7, suma("1,2,1001,4"))
 
+	def testCadenaConDelimitadorDeCualquierLongitud (self):
+		self.assertEqual(6, suma("//[***]\n1***2***3"))
+
+	def testCadenaConDelimitadorDeCualquierLongitudYSalto (self):
+		self.assertEqual(6, suma("//[***]\n1***2\n***3"))
+
 	def testCadenaConVariosDelimitadores (self):
-		self.assertEqual(6, suma("//[..]\n1..2..3"))
+		self.assertEqual(6, suma("//[*][%]\n1*2%3"))
+
+	def testCadenaConVariosDelimitadoresDeCualquierLongitud (self):
+		self.assertEqual(6, suma("//[:;.][---]\n1:;.2---3"))
 
 if __name__ == '__main__':
 	unittest.main()
