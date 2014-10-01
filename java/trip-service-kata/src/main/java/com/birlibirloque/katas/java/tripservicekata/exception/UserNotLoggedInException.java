5c3f5eb53e05ad2ed487dc85e89bcc0dc500b13e
birlibirloque <manuel.kstro@gmail.com>
2014-10-01 18:54:49 +0200

    Transformed to RuntimeException

--- java/trip-service-kata/src/main/java/com/birlibirloque/katas/java/tripservicekata/exception/UserNotLoggedInException.java
+++ java/trip-service-kata/src/main/java/com/birlibirloque/katas/java/tripservicekata/exception/UserNotLoggedInException.java
@@ -1,6 +1,6 @@
 package com.birlibirloque.katas.java.tripservicekata.exception;
 
-public class UserNotLoggedInException extends Exception {
+public class UserNotLoggedInException extends RuntimeException {
 
 	private static final long serialVersionUID = 8959479918185637340L;
 
